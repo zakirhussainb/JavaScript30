774604cd4624523991e9bea104b66242a805665f
zakirhussainb <zakir@xedflix.com>
2018-08-26 16:34:51 +0530

    Made changes

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -6,7 +6,6 @@ function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if(!audio) return;
-
   key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
